39702a34ecfe01adce52f6ea35cf35a33577eb4e
WessonChen <hhchenweicheng@gmail.com>
2020-01-03 20:54:51 +1100

    Use @PostConstruct and @PreDestroy in bean lifecycle.

--- src/main/java/com/example/SpringBasics/basics/BinarySearchImpl.java
+++ src/main/java/com/example/SpringBasics/basics/BinarySearchImpl.java
@@ -1,15 +1,22 @@
 package com.example.SpringBasics.basics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 //@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private SortAlgorithm sortAlgorithm;
 
@@ -22,5 +29,15 @@ public class BinarySearchImpl {
         int[] sortedNumbers = sortAlgorithm.sort(numbers);
         System.out.println(sortedNumbers);
         return numbersToSearchFor;
+    }
+
+    @PostConstruct
+    public void postCons(){
+        logger.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDes(){
+        logger.info("preDestroy");
     }
 }
