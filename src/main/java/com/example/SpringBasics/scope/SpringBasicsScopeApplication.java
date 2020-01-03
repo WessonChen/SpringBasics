ff31621c816c0d21f7bfd368c747626bdba926d7
WessonChen <hhchenweicheng@gmail.com>
2020-01-03 20:37:29 +1100

    @ComponentScans and @ComponentScan when main class and components are not in the same package.

--- src/main/java/com/example/SpringBasics/scope/SpringBasicsScopeApplication.java
+++ src/main/java/com/example/SpringBasics/scope/SpringBasicsScopeApplication.java
@@ -1,13 +1,19 @@
 package com.example.SpringBasics.scope;
 
+import com.example.SpringBasics.basics.BinarySearchImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 
 @SpringBootApplication
+@ComponentScans({
+		@ComponentScan(basePackageClasses = BinarySearchImpl.class)
+})
 public class SpringBasicsScopeApplication {
 
 	private static Logger LOGGER =
@@ -24,10 +30,17 @@ public class SpringBasicsScopeApplication {
 		PersonDAO personDAO1 =
 				applicationContext.getBean(PersonDAO.class);
 
+		BinarySearchImpl binarySearch =
+				applicationContext.getBean(BinarySearchImpl.class);
+
+		int result = binarySearch.binarySearch(new int[] {1, 2, 3}, 0);
+
 		LOGGER.info("{}", personDAO);
 		LOGGER.info("{}", personDAO.getJdbcConnection());
 
 		LOGGER.info("{}", personDAO1);
 		LOGGER.info("{}", personDAO1.getJdbcConnection());
+
+		LOGGER.info("{}", result);
 	}
 }
